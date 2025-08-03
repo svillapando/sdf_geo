3be0f2f9254a75eb5fb2384bfeb3d18a03730cdf
Soliman Villapando <svillapando@ucsd.edu>
2025-08-03 00:49:28 -0700

    Generalized box to include rotation

--- sdf_geometry/primitives.py
+++ sdf_geometry/primitives.py
@@ -1,18 +1,33 @@
 import numpy as np
 import csdl_alpha as csdl
+from scipy.spatial.transform import Rotation
 
 
+def sdf_box(p, center, half_size, rotation_angles, degrees=True):
 
-def sdf_box(p, bmin, bmax):
-    bmin = np.array(bmin)
-    bmax = np.array(bmax)
-    center = np.broadcast_to(0.5 * (bmin + bmax), p.shape)
-    half_size = np.broadcast_to(0.5 * (bmax - bmin), p.shape)
-    q = csdl.absolute(p - center) - half_size
+    # Compute rotation matrix from global x, y, z axes
+    R = Rotation.from_euler('xyz', rotation_angles, degrees=degrees).as_matrix()
+    R_T = R.T  
+
+    center_bcast = np.broadcast_to(center, p.shape)
+    p_local = p - center_bcast  # (..., 3)
+ 
+    last_axis = len(p_local.shape) - 1
+    p_rot = csdl.tensordot(p_local, R_T, axes=([last_axis], [1]))  # (..., 3)
+
+    half_bcast = np.broadcast_to(half_size, p.shape)
+    q = csdl.absolute(p_rot) - half_bcast
+
+    # Compute distance to surface (only if outside)
     q_clip = csdl.maximum(q, np.broadcast_to(0.0, q.shape))
-    last_axis_qclip = len(q_clip.shape) - 1
-    last_axis_q = len(q.shape) -1
-    return csdl.norm(q_clip,axes=(last_axis_qclip,)) + csdl.minimum(csdl.maximum(q, axes=(last_axis_q,)), np.broadcast_to(0.0, (100,100,100)))
+    norm_val = csdl.norm(q_clip, axes=(len(q.shape)-1,))
+
+    # Compute distance to surface (only if inside)
+    q_max = csdl.maximum(q, axes=(len(q.shape)-1,))
+    correction = csdl.minimum(q_max, np.broadcast_to(0.0, q_max.shape))
+
+    # If inside, norm_val -> 0, correction -> negative. If outside, norm_val -> positive, correction -> 0 
+    return norm_val + correction
 
 def sdf_sphere(p, center, radius):
     center = np.broadcast_to(center, p.shape)
