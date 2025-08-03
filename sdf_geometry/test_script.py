8b41a8f28417e7122a8e4ec32f4bc0bde085b9a8
Soliman Villapando <svillapando@ucsd.edu>
2025-08-02 17:22:41 -0700

    Revised operations to be csdl compatible

--- sdf_geometry/test_script.py
+++ sdf_geometry/test_script.py
@@ -1,9 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import csdl_alpha as csdl
 
 from sdf_geometry.primitives import sdf_box, sdf_sphere
 from sdf_geometry.operations import smooth_union, smooth_intersection, smooth_subtraction
 from sdf_geometry.visualization import plot_2d_slice, plot_3d_isosurface
+
+recorder = csdl.Recorder(inline = True)
+recorder.start()
 
 # ==== USER DEFINED PARAMETERS === 
 
@@ -16,7 +19,7 @@ x = np.linspace(-5, 5, res)
 y = np.linspace(-2, 2, res)
 z = np.linspace(-2, 2, res)
 X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
-P = np.stack([X, Y, Z], axis=-1)  # shape: (res, res, res, 3)
+P = csdl.Variable(value = np.stack([X, Y, Z], axis=-1))  # shape: (res, res, res, 3)
 
 # Define shapes (sphere and box)
 c = np.zeros(3)                          #Sphere center
@@ -35,16 +38,16 @@ phi_sphere = sdf_sphere(P, center = c, radius = R)
 # Compute Operation
 if operation == 1:
     phi_union = smooth_union(phi_box, phi_sphere)
-    plot_2d_slice(phi_union, x, y, title="2D Slice of Union SDF")
-    plot_3d_isosurface(phi_union, x, y, z)
+    plot_2d_slice(phi_union.value, x, y, title="2D Slice of Union SDF")
+    plot_3d_isosurface(phi_union.value, x, y, z)
 elif operation == 2:
     phi_intersection = smooth_intersection(phi_box, phi_sphere)
-    plot_2d_slice(phi_intersection, x, y, title="2D Slice of Intersection SDF")
-    plot_3d_isosurface(phi_intersection, x, y, z)
+    plot_2d_slice(phi_intersection.value, x, y, title="2D Slice of Intersection SDF")
+    plot_3d_isosurface(phi_intersection.value, x, y, z)
 else:
     phi_subtraction = smooth_subtraction(phi_sphere, phi_box)
-    plot_2d_slice(phi_subtraction, x, y, title="2D Slice of Subtraction SDF")
-    plot_3d_isosurface(phi_subtraction, x, y, z)
+    plot_2d_slice(phi_subtraction.value, x, y, title="2D Slice of Subtraction SDF")
+    plot_3d_isosurface(phi_subtraction.value, x, y, z)
 
 
 
