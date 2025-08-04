216e0f1004e73aece9b6286b339f2f412c76d677
Soliman Villapando <svillapando@ucsd.edu>
2025-08-04 15:47:30 -0700

    pip-installable PRISM package

--- prism/test_script.py
+++ prism/test_script.py
@@ -1,9 +1,9 @@
 import numpy as np
 import csdl_alpha as csdl
 
-from sdf_geometry.primitives import sdf_box, sdf_sphere, sdf_plane, sdf_capsule
-from sdf_geometry.operations import smooth_union, smooth_intersection, smooth_subtraction
-from sdf_geometry.visualization import plot_2d_slice, plot_3d_isosurface
+import prism.primitives as prim
+import prism.operations as op
+from prism.visualization import plot_2d_slice, plot_3d_isosurface
 
 recorder = csdl.Recorder(inline=True)
 recorder.start()
@@ -11,7 +11,7 @@ recorder.start()
 # ==== USER DEFINED PARAMETERS === 
 
 # Choose Operation 1: Union, 2: Intersection, 3: Subtraction (A - B)
-operation = 1
+operation = 2
 
 # Create a 3D grid to evaluate SDF
 res = 100
@@ -38,32 +38,33 @@ p2 = np.array([2, 0, 1])                    # Capsule end point
 # === END USER DEFINED PARAMETERS ===
 
 # Build SDF functions
-phi_box_fn = sdf_box(center, half_size, rotation_angles)
-phi_sphere_fn = sdf_sphere(c, R)
-phi_plane_fn = sdf_plane(p0, n)
-phi_capsule_fn = sdf_capsule(p1, p2, R)
+phi_box_fn = prim.sdf_box(center, half_size, rotation_angles)
+phi_sphere_fn = prim.sdf_sphere(c, R)
+phi_plane_fn = prim.sdf_plane(p0, n)
+phi_capsule_fn = prim.sdf_capsule(p1, p2, R)
 
 
 # Composite operations
+point = np.array([2, 0, -1])
 if operation == 1:
-    sdf_fn = smooth_union(phi_box_fn, phi_capsule_fn)
+    sdf_fn = op.smooth_union(phi_box_fn, phi_capsule_fn)
     phi = sdf_fn(P)
     plot_2d_slice(phi.value, x, y, title="2D Slice of Union SDF")
     plot_3d_isosurface(phi.value, x, y, z)
+    print('Phi value:', sdf_fn(point).value)
 
 elif operation == 2:
-    sdf_fn = smooth_intersection(phi_sphere_fn, phi_plane_fn)
+    sdf_fn = op.smooth_intersection(phi_box_fn, phi_capsule_fn)
     phi = sdf_fn(P)
     plot_2d_slice(phi.value, x, y, title="2D Slice of Intersection SDF")
     plot_3d_isosurface(phi.value, x, y, z)
+    print('Phi value:', sdf_fn(point).value)
 
 else:
-    sdf_fn = smooth_subtraction(phi_box_fn, phi_capsule_fn)
+    sdf_fn = op.smooth_subtraction(phi_box_fn, phi_capsule_fn)
     phi = sdf_fn(P)
     plot_2d_slice(phi.value, x, y, title="2D Slice of Subtraction SDF")
     plot_3d_isosurface(phi.value, x, y, z)
+    print('Phi value:', sdf_fn(point).value)
 
-# Test individual point
-point = np.array([2, 0, -1])
-sample = phi_capsule_fn(point)
-print('Phi value:', sample.value)
+
